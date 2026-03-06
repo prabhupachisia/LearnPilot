ba10d1eea923045e97c3a4eccbf98fe3ba143e0c
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2026-03-06 14:12:18 +0530

    Updated env path

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,10 @@
 import os
 from supabase import create_client
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
