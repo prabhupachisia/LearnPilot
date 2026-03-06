7382c2eb770f20c1e6bb8b788b66e2d87b4db3c8
Vivek Rai <vkkr1430@gmail.com>
2026-03-06 13:55:40 +0530

    abcd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,8 @@ import Explore from "./pages/Explore";
 import PathBuilder from "./pages/PathBuilder";
 import LearningPaths from "./pages/LearningPaths";
 import ProtectedLayout from "./components/ProtectedLayout";
-<<<<<<< HEAD
-import DocChat from "./pages/DocChat";
-=======
 import LearningView from "./components/LearningView";
->>>>>>> 7d460dd3a709f508951c80a8a4c34db9dc947816
+import DocChat from "./pages/DocChat";
 
 const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -32,11 +29,9 @@ function App() {
               <Route path="/explore" element={<Explore />} />
               <Route path="/pathbuilder" element={<PathBuilder />} />
               <Route path="/learning-paths" element={<LearningPaths />} />
-<<<<<<< HEAD
-              <Route path="/chat" element={<DocChat />} />
-=======
               <Route path="/learning-view/:pathId" element={<LearningView />} />
->>>>>>> 7d460dd3a709f508951c80a8a4c34db9dc947816
+              <Route path="/chat" element={<DocChat />} />
+
             </Route>
           </Routes>
         </BrowserRouter>
